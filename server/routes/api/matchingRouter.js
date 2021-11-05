07d389b5cc763bf0b7c89de7a5c55ca7fb87b182
mskim9967 <mskim9967@gmail.com>
2021-11-06 04:08:34 +0900

    fix(api) 매칭 조회 시 참가 신청한 유저 정보도 같이 불러오도록 수정

--- server/routes/api/matchingRouter.js
+++ server/routes/api/matchingRouter.js
@@ -33,6 +33,12 @@ router.get('/search', async (req, res) => {
         for(let result of results) {
             sql = 'select * from matching_join where matching_id = ?'; 
             let [joinRequests] = await dbConn.query(sql, [result.id]);
+
+            for(let joinRequest of joinRequests) {
+                sql = 'select * from user_kakao where id = ?'; 
+                let [userInfo] = await dbConn.query(sql, [joinRequest.guest_id]);
+                joinRequest.guest = userInfo[0];
+            }
             result.joinRequests = joinRequests;
         }
 
@@ -93,6 +99,12 @@ router.get('/:id', async (req, res) => {
         for(let result of results) {
             sql = 'select * from matching_join where matching_id = ?'; 
             let [joinRequests] = await dbConn.query(sql, [result.id]);
+
+            for(let joinRequest of joinRequests) {
+                sql = 'select * from user_kakao where id = ?'; 
+                let [userInfo] = await dbConn.query(sql, [joinRequest.guest_id]);
+                joinRequest.guest = userInfo[0];
+            }
             result.joinRequests = joinRequests;
         }
 
@@ -102,6 +114,7 @@ router.get('/:id', async (req, res) => {
         });
 
     } catch(err) {
+        console.error(err);
         return res.status(400).json({
             status: "NOT FOUND"
         });
