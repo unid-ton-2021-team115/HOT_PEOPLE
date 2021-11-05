baafdf69b89b41ea426b7c291304af4217928cb2
mskim9967 <mskim9967@gmail.com>
2021-11-06 03:14:00 +0900

    feat(api) 매칭 참가 취소 기능 구현

--- server/routes/api/matchingJoinRouter.js
+++ server/routes/api/matchingJoinRouter.js
@@ -24,7 +24,7 @@ router.post('/', kakaoAuth.checkLogin, async (req, res) => {
 
 router.get('/search', async (req, res) => {
     try {
-        let sql = 'select * from matching_id where'; 
+        let sql = 'select * from matching_join where'; 
         for(key in req.query) sql += ` ${key} = ? AND`;
         sql = sql.substr(0, sql.length - 3);
         let [results] = await dbConn.query(sql, Object.values(req.query));
@@ -35,6 +35,7 @@ router.get('/search', async (req, res) => {
         });
 
     } catch(err) {
+        console.error(err);
         return res.status(400).json({
             status: "NOT FOUND"
         });
@@ -43,7 +44,7 @@ router.get('/search', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        let sql = 'select * from matching_id where id = ?'; 
+        let sql = 'select * from matching_join where id = ?'; 
         let [results] = await dbConn.query(sql, [req.params.id]);
 
         return res.status(200).json({
@@ -52,6 +53,34 @@ router.get('/:id', async (req, res) => {
         });
 
     } catch(err) {
+        console.error(err);
+        return res.status(400).json({
+            status: "NOT FOUND"
+        });
+    }
+});
+
+router.delete('/:id', kakaoAuth.checkLogin, async (req, res) => {
+    try {
+        let sql = 'select * from matching_join where id = ?'; 
+        let [matching_join] = await dbConn.query(sql, [req.params.id]);
+        matching_join = matching_join[0];
+
+        if(matching_join.guest_id !== req.user.id)
+            return res.status(400).json({
+                status: "YOU'RE NOT USER"
+            });
+
+        sql = 'delete from matching_join where id = ?'; 
+        let [results] = await dbConn.query(sql, [req.params.id]);
+        
+        return res.status(200).json({
+            status: "OK",
+            data: results[0]
+        });
+
+    } catch(err) {
+        console.error(err);
         return res.status(400).json({
             status: "NOT FOUND"
         });
