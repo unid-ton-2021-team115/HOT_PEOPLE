614b75ece7069a62b837c28971ed0bdce4ca8b41
DivisonOfficer <41609506+DivisonOfficer@users.noreply.github.com>
2021-11-06 00:51:29 +0900

    gps

--- app/src/main/java/ins/hands/unid/MainActivity.kt
+++ app/src/main/java/ins/hands/unid/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.kakao.sdk.user.UserApiClient
 import ins.hands.unid.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -15,7 +17,7 @@ class MainActivity : BaseActivity() {
     val viewModel : MainViewModel  by viewModel()
     var hash = ""
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        requestPermission {  }
         super.onCreate(savedInstanceState)
 
 
@@ -37,10 +39,29 @@ class MainActivity : BaseActivity() {
                 Log.i(TAG, "로그인 성공 ${token.accessToken}")
                 viewModel.getUserData(token.accessToken) {
                     runOnUiThread {
-                        startActivity(Intent())
+                        startActivity(Intent(this,HomeActivity::class.java).apply{
+
+                        })
                     }
                 }
             }
         }
+    }
+    private fun requestPermission(func: () -> Unit) {
+        val permissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                func()
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                //"PermissionDenied".showShortToast()
+                finishAffinity()
+            }
+        }
+        TedPermission.with(this)
+            .setPermissionListener(permissionListener)
+            .setDeniedMessage("If you reject permission,you can not use this service\\n\\nPlease turn on permissions at [Setting] > [Permission]")
+            .setPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION)
+            .check()
     }
 }
