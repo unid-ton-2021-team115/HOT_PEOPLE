68668d0af4575526ea6a7aa50a1ef8285e5b0c18
mskim9967 <mskim9967@gmail.com>
2021-11-06 16:42:12 +0900

    feat(api) 플레이스 타입으로 플레이스 조회하는 기능 구현

--- server/routes/api/placeRouter.js
+++ server/routes/api/placeRouter.js
@@ -29,6 +29,32 @@ router.get('/search', async (req, res) => {
     }
 });
 
+router.get('/type/:type', async (req, res) => {
+    try {
+        let sql = 'select p.* from place_types t, place p where p.id = t.place_id and t.type = ?'; 
+        let [results] = await dbConn.query(sql, [req.params.type]);
+
+        for(let result of results) {
+            sql = 'select * from place_types where place_id = ?'; 
+            let [place_types] = await dbConn.query(sql, [result.id]);
+            let types = [];
+            for(let place_type of place_types)
+                types.push(place_type.type)
+            result.types = types;
+        } 
+
+        return res.status(200).json({
+            status: "OK",
+            data: results
+        });
+
+    } catch(err) {
+        return res.status(400).json({
+            status: "NOT FOUND"
+        });
+    }
+});
+
 router.get('/:id', async (req, res) => {
     try {
         let sql = 'select * from place where id = ?'; 
@@ -42,7 +68,7 @@ router.get('/:id', async (req, res) => {
                 types.push(place_type.type)
             result.types = types;
         } 
-        
+
         return res.status(200).json({
             status: "OK",
             data: results[0]
