b4b5a5bcac31d9ae27fdcab23872de1767280c2a
coldreason <coldreason@webmail.korea.ac.kr>
2021-11-06 20:49:21 +0900

    update night mode

--- app/src/main/java/ins/hands/unid/MainActivity.kt
+++ app/src/main/java/ins/hands/unid/MainActivity.kt
@@ -1,15 +1,17 @@
 package ins.hands.unid
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
+import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import ins.hands.unid.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import com.kakao.sdk.common.util.Utility
+
 
 class MainActivity : BaseActivity() {
     val TAG = "MainActivity"
@@ -17,6 +19,7 @@ class MainActivity : BaseActivity() {
     val viewModel : MainViewModel  by viewModel()
     var hash = ""
     override fun onCreate(savedInstanceState: Bundle?) {
+
         requestPermission {  }
         super.onCreate(savedInstanceState)
 
@@ -39,7 +42,7 @@ class MainActivity : BaseActivity() {
                 Log.i(TAG, "로그인 성공 ${token.accessToken}")
                 viewModel.getUserData(token.accessToken) {
                     runOnUiThread {
-                        startActivity(Intent(this,HomeActivity::class.java).apply{
+                        startActivity(Intent(this, HomeActivity::class.java).apply {
 
                         })
                     }
@@ -61,7 +64,12 @@ class MainActivity : BaseActivity() {
         TedPermission.with(this)
             .setPermissionListener(permissionListener)
             .setDeniedMessage("If you reject permission,you can not use this service\\n\\nPlease turn on permissions at [Setting] > [Permission]")
-            .setPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION)
+            .setPermissions(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
             .check()
     }
 }
