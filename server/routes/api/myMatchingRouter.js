6d639f2b7632bd96f33ce5c15878438cdd5975ef
mskim9967 <mskim9967@gmail.com>
2021-11-06 18:01:55 +0900

    fix(api) my_matching guset 정보 추가

--- server/routes/api/myMatchingRouter.js
+++ server/routes/api/myMatchingRouter.js
@@ -8,12 +8,25 @@ router.get('/:status', passport.authenticate('kakao-token'), async (req, res) =>
         let sql = 'select * from matching where host_id = ? and status = ?'; 
         let [results] = await dbConn.query(sql, [req.user.id, req.params.status]);
 
+        for(let result of results) {
+            sql = 'select * from matching_join where matching_id = ?'; 
+            let [joinRequests] = await dbConn.query(sql, [result.id]);
+
+            for(let joinRequest of joinRequests) {
+                sql = 'select * from user_kakao where id = ?'; 
+                let [userInfo] = await dbConn.query(sql, [joinRequest.guest_id]);
+                joinRequest.guest = userInfo[0];
+            }
+            result.joinRequests = joinRequests;
+        }
+
         return res.status(200).json({
             status: "OK",
             data: results 
         });
 
     } catch(err) {
+        console.error(err);
         return res.status(400).json({
             status: "NOT FOUND"
         });
